e7029364bd025d7ccd6dae1a820ed05b8a1ba7b5
Garrett Lee <rgl10d@gmail.com>
2020-10-13 14:21:45 -0400

    added more html and connected the js file to index

---
+++ script.js
@@ -0,0 +1,27 @@
+$(document).ready(function(){
+    console.log("here it is.")
+
+    //DOM Variables
+    
+    //JS Variables
+    // var queryurl
+    //Function Definitons
+
+    //Function Calls
+
+    //Event Listeners
+
+    // $(#search-button).on("click", function(){
+    //     var cityt = $(this).attr("data-person");
+    //     $.ajax({
+    //         url: queryurl,
+    //         method: "GET"
+    //     }).then(function(response){
+            
+    //     })
+
+    // })
+
+
+})
+    
