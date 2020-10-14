7556c93390d05cd96809f57730715a94d81ab1e8
Garrett Lee <rgl10d@gmail.com>
2020-10-14 17:25:14 -0400

    added weather icons to the weather details and comments to the javascript and css files

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 $(document).ready(function(){
-
     var searchBtn = $("#search-button");
     var searchAside = $("#aside-row");
     var now = moment().format("MM/DD/YYYY");
@@ -8,26 +7,29 @@ $(document).ready(function(){
                     moment().add(3, "days").format("MM/DD/YYYY"),
                     moment().add(4, "days").format("MM/DD/YYYY"),
                     moment().add(5, "days").format("MM/DD/YYYY")
-                ];
+                    ];
 
-    //On click event that retrieves and displays all weather data
+    //On click event on the search button
     $(searchBtn).on("click", function(){
         var cityName = $("#search-bar").val();
         var queryURL = "https://api.openweathermap.org/data/2.5/weather?q="
                        + cityName + "&units=imperial&appid=c683bc51fb96f85dead080d9ec469b07";
         localStorage.setItem("City", cityName);
+
+        //Creates buttons from previous search entries
         var pastSearchEl = $("<div>").attr("class", "col-sm-12");
         var pastSearchBtn = $("<button>").attr("class", "past-search").text(cityName);
-
         searchAside.append(pastSearchEl);
         pastSearchEl.append(pastSearchBtn);
 
+        //AJAX API call to retrieves the weather data
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
-            console.log(response);
-            var iconImg = $("<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png>");
+
+            //Defining variables
+            var iconImg = $("<img src=http://openweathermap.org/img/wn/" + response.weather[0].icon + ".png>");
             var latitude = response.coord.lat;
             var longitude = response.coord.lon;
             var uvURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" 
@@ -35,8 +37,10 @@ $(document).ready(function(){
                          + longitude + "&appid=c683bc51fb96f85dead080d9ec469b07";
             var fiveDayURL = "http://api.openweathermap.org/data/2.5/forecast?q="
                              + cityName + "&units=imperial&appid=c683bc51fb96f85dead080d9ec469b07";
-            console.log(iconImg);
-            $("#city-name").html("<h3>" + response.name + " (" + now + ") " + iconImg + "</h3>");
+            
+            //Populating the elements with the searched city name's weather
+            $("#city-name").html("<h3>" + response.name + " (" + now + ")</h3>");
+            $("#city-name").append(iconImg);
             $("#temperature").text("Temperature: " + response.main.temp + " °F");
             $("#humidity").text("Humidity: " + response.main.humidity + "%");
             $("#wind-speed").text("Wind Speed: " + response.wind.speed + " MPH");
@@ -52,7 +56,6 @@ $(document).ready(function(){
                 url: fiveDayURL,
                 method: "GET"
             }).then(function(response){
-                console.log(response);
 
                 //Clears out the boxes when another search occurs
                 $("#five-day").empty();
@@ -63,7 +66,7 @@ $(document).ready(function(){
                     "class": "col-sm-12",
                     "id": "five-day-title"
                 });
-                var fiveHeader = $("<h4>").text("5-Day Forecast");
+                var fiveHeader = $("<h4>").text("5-Day Forecast:");
 
                 forecastRow.append(fiveTitleEl);
                 fiveTitleEl.append(fiveHeader);
@@ -78,13 +81,14 @@ $(document).ready(function(){
                             "id": boxID
                         });
                         var forecastDate = $("<h5>").attr("class", "forecast-date").text(nextFive[i]);
-                        // var forecastIcon;
+                        var forecastIcon = $("<img src=http://openweathermap.org/img/wn/" + response.list[i].weather[0].icon + ".png>");
                         var forecastTemp = $("<p>").attr("class", "forecast-details").text("Temp: " + response.list[i].main.temp + " °F");
                         var forecastHumid = $("<p>").attr("class", "forecast-details").text("Humidity: " + response.list[i].main.humidity + "%");
             
             
                         forecastRow.append(forecastBox);
                         forecastBox.append(forecastDate);
+                        forecastBox.append(forecastIcon);
                         forecastBox.append(forecastTemp);
                         forecastBox.append(forecastHumid);
             
@@ -94,8 +98,6 @@ $(document).ready(function(){
                 //Forecast box function call
                 forecastRender();
             })
-
-            
 
         })
 
